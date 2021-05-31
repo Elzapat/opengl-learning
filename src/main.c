e6d0a09e0fa00ce53b6618ae0ad55f918a2c23bd
Morgan Van Amerongen <mistigri.van@gmail.com>
2021-05-31 16:04:10 +0200

    textures

--- src/main.c
+++ src/main.c
@@ -4,12 +4,17 @@
 #include <GLFW/glfw3.h>
 #include <math.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/stb_image.h"
+
 #include "../include/shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
 unsigned int create_first_triangle();
 unsigned int create_second_triangle();
+unsigned int create_rectangle();
+unsigned int create_benoit_texture();
 
 int main() {
     glfwInit();
@@ -33,26 +38,18 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    /* unsigned int indices[] = { */
-    /*     0, 1, 3, */
-    /*     1, 2, 3 */
-    /* }; */
-
-    /* unsigned int orange_shader_program = create_shader_program( */
-    /*         "src/vertex_shader.vert", "src/orange_frag_shader.frag"); */
-    /* unsigned int yellow_shader_program = create_shader_program( */
-    /*         "src/vertex_shader.vert", "src/yellow_frag_shader.frag"); */
-
     unsigned int shader_program = create_shader_program(
             "src/vertex_shader.vert", "src/frag_shader.frag");
+    unsigned int yellow_shader_program = create_shader_program(
+            "src/vertex_shader.vert", "src/yellow_frag_shader.frag");
+    unsigned int texture_shader_program = create_shader_program(
+            "src/tex_vertex_shader.vert", "src/tex_frag_shader.vert");
 
     unsigned int first_triangle = create_first_triangle();
     unsigned int second_triangle = create_second_triangle();
+    unsigned int rectangle = create_rectangle();
 
-    /* unsigned int EBO; */
-    /* glGenBuffers(1, &EBO); */
-    /* glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); */
-    /* glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); */
+    unsigned int benoit_texture = create_benoit_texture();
 
     /* glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); */
 
@@ -66,23 +63,49 @@ int main() {
 
         glUseProgram(shader_program);
 
+        glBindVertexArray(first_triangle);
+
         float time_value = glfwGetTime();
-        float green_value = (sin(time_value) / 2.0f) + 0.5f;
-        int vertex_color_location = glGetUniformLocation(shader_program, "ourColor");
-        if (vertex_color_location == -1) {
-            printf("Error retrieving the vertex color location\n");
+        float horizontal_offset = sin(time_value) / 2.0f;
+        float vertical_offset = cos(time_value) / 2.0f;
+
+        int horizontal_offset_location = glGetUniformLocation(
+                shader_program, "horizontal_offset");
+        if (horizontal_offset_location == -1) {
+            printf("Error retrieving the horizontal offset location\n");
+            return -1;
+        }
+        int vertical_offset_location = glGetUniformLocation(
+                shader_program, "vertical_offset");
+        if (horizontal_offset_location == -1) {
+            printf("Error retrieving the vertical offset location\n");
             return -1;
         }
 
-        glUniform4f(vertex_color_location, 0.0f, green_value, 0.0f, 1.0f);
+        glUniform1f(horizontal_offset_location, horizontal_offset);
+        glUniform1f(vertical_offset_location, vertical_offset);
 
-        glBindVertexArray(first_triangle);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* glUseProgram(yellow_shader_program); */
+        glUseProgram(yellow_shader_program);
 
         glBindVertexArray(second_triangle);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+        /* unsigned int texture_location = glGetUniformLocation( */
+        /*         texture_shader_program, "ourTexture"); */
+        /* if (texture_location == -1) { */
+        /*     printf("Error retrieving texture location"); */
+        /*     return -1; */
+        /* } */
+        /* glBindTexture(GL_TEXTURE_2D, benoit_texture); */
+        glUseProgram(texture_shader_program);
+        glBindVertexArray(rectangle);
+        /* glDrawArrays(GL_TRIANGLES, 0, 3); */
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        /* glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rectangle); */
+        /* glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); */
 
         /* glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); */
         /* glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); */
@@ -91,6 +114,10 @@ int main() {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &first_triangle);
+    glDeleteVertexArrays(1, &second_triangle);
+    glDeleteBuffers(1, &rectangle);
 
     glfwTerminate();
     return 0;
@@ -107,9 +134,9 @@ void process_input(GLFWwindow* window) {
 
 unsigned int create_first_triangle() {
     float vertices[] = {
-        -0.5f, 0.5f, 0.0f,
-         0.5f, 0.5f, 0.0f,
-         0.0f, 0.0f, 0.0f
+        -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
     unsigned int VBO;
@@ -121,8 +148,12 @@ unsigned int create_first_triangle() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     return VAO;
 }
@@ -147,4 +178,71 @@ unsigned int create_second_triangle() {
     glEnableVertexAttribArray(0);
 
     return VAO;
+}
+
+unsigned int create_rectangle() {
+    float vertices[] = {
+        // position         // colors         // texture coords
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right 
+        -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -1.0f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+            (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+            (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+            (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    return VAO;
+}
+
+unsigned int create_benoit_texture() {
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nb_channels;
+    unsigned char* image_data = stbi_load("assets/benoit.jpg", &width, &height,
+            &nb_channels, 0);
+
+    if (!image_data) {
+        printf("Error loading texture\n");
+        exit(-1);
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+            GL_UNSIGNED_BYTE, image_data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(image_data);
+
+    return texture;
 }
--- src/vertex_shader.vert
+++ src/vertex_shader.vert
@@ -1,7 +1,14 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+uniform float horizontal_offset;
+uniform float vertical_offset;
+
+out vec3 ourColor;
 
 void main() {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x + horizontal_offset, aPos.y + vertical_offset, aPos.z, 1.0);
+    ourColor = vec3(aPos.x + horizontal_offset, aPos.y + vertical_offset, aPos.z);
 }
